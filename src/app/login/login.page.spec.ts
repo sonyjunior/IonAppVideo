380a1c872e4338a78ce7969e6b9984f8a04d7766
nizzola <marcio@nizzola.com.br>
2021-06-05 10:05:55 -0300

    implementação do login

--- src/app/login/login.page.spec.ts
+++ src/app/login/login.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { BackFuturePage } from './back-future.page';
+import { LoginPage } from './login.page';
 
-describe('BackFuturePage', () => {
-  let component: BackFuturePage;
-  let fixture: ComponentFixture<BackFuturePage>;
+describe('LoginPage', () => {
+  let component: LoginPage;
+  let fixture: ComponentFixture<LoginPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ BackFuturePage ],
+      declarations: [ LoginPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(BackFuturePage);
+    fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
